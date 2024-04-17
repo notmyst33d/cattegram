6854c4b5d7092156ab1034730d28e935183b29a4
Myst33d <notmyst33d@gmail.com>
2024-04-17 16:21:47 +0600

    cattl: Add int24 support and fix bytes extended length deserialization

--- cattl/src/bytes_buffer.rs
+++ cattl/src/bytes_buffer.rs
@@ -64,16 +64,36 @@ impl BytesBuffer {
     impl_primitive_write!(write_long, 8, i64);
     impl_primitive_write!(write_int128, 16, i128);
 
+    // Thanks Durov, very cool
+    #[inline]
+    pub fn read_int24(&mut self) -> Result<i32, TlError> {
+        if self.position + 3 > self.data.len() {
+            return Err(TlError::NotEnoughData);
+        }
+        self.position += 3;
+        let mut value = 0;
+        unsafe {
+            value |= (self.data.as_ptr().add(self.position - 3) as *const i8).read_unaligned() as i32;
+            value |= ((self.data.as_ptr().add(self.position - 2) as *const i8).read_unaligned() as i32) << 8;
+            value |= ((self.data.as_ptr().add(self.position - 1) as *const i8).read_unaligned() as i32) << 16;
+        }
+        Ok(value)
+    }
+
     pub fn read_bytes(&mut self) -> Result<Vec<u8>, TlError> {
         if self.position + 1 > self.data.len() {
             return Err(TlError::NotEnoughData);
         }
-        let length = self.read_byte()? as usize;
+        let mut length = self.read_byte()? as usize;
+        if (length & 0xff) == 0xfe {
+            length = self.read_int24()? as usize;
+        }
         if self.position + length > self.data.len() {
             return Err(TlError::NotEnoughData);
         }
-        self.position += length;
-        Ok(self.data[self.position - length..self.position].to_vec())
+        let padding = 4 - (length + 1).rem_euclid(4);
+        self.position += length + padding;
+        Ok(self.data[self.position - (length + padding)..self.position - padding].to_vec())
     }
 
     pub fn write_bytes(&mut self, value: &[u8]) {
@@ -85,7 +105,7 @@ impl BytesBuffer {
         unsafe {
             copy_nonoverlapping(value.as_ptr(), self.data.as_ptr().add(self.position - value.len()) as *mut u8, value.len())
         }
-        let padding = (-(value.len() as i32 + 1)).rem_euclid(4);
+        let padding = 4 - (value.len() + 1).rem_euclid(4);
         for _ in 0..padding {
             self.write_byte(0);
         }
